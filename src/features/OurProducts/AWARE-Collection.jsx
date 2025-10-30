ff90b732c12f99e673c82074199e50762c46a0c1
Franz Pamorada <fopamorada@gmail.com>
2025-10-31 02:56:56 +0800

    aware and wwp styling adjustments

--- src/features/OurProducts/AWARE-Collection.jsx
+++ src/features/OurProducts/AWARE-Collection.jsx
@@ -190,7 +190,7 @@ const AwareCollectionSection = () => {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         {/* Header */}
         <div className='text-center mb-12'>
-          <h2 className='text-3xl font-extrabold text-gray-900 uppercase tracking-wider'>
+          <h2 className='pt-10 text-3xl font-extrabold text-gray-900 uppercase tracking-wider'>
             THE AWARE COLLECTION
           </h2>
           <p className='mt-4 text-sm  text-gray-600'>
